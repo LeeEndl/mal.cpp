8f922d9f8f436010f41cdf699c0e4b6a5fb22cd8
LeeEndl <gavinolson71@gmail.com>
2023-12-06 22:29:09 -0600

    lambda callback on `anime_get()` & optimizations

--- include/mal/mal.hpp
+++ include/mal/mal.hpp
@@ -8,62 +8,30 @@
 
 constexpr const char* api_v = "v4"; /* API version. */
 
-/* for CURLOPT_WRITEFUNCTION. this was made by libcurl not me. */
-size_t write_data(char* data, size_t size, size_t bytes, std::string* outcome) noexcept {
-	outcome->append(std::move(data), size * bytes);
-	return size * bytes;
-}
-
-/* replace string length data rather std::replace() replacing only char */
-std::string replace(const std::string& str, char replace, std::string with) noexcept {
-	std::string mstr{};
-	for (const char& c : str)
-		(c == replace) ? mstr += with : mstr += c;
-	return std::move(mstr);
-}
-
-/*
-* GET a specfic anime via name and store it inside JSON format.
- * support spaces, mis-spelling. however this is not enhanced; wrong result is possible.
- * @param results the number of results. this is useful for getting a whole anime series collection like one piece movies, fate series, ect
- *   by default the value is 1 meaning it'll only GET the most relevant search based on the name.
- *   this can also be used as a iterator if you only wanna get movies or tv series or specials,
- *   this feature is not included however can be implemented manually.
-*/
-std::vector<nlohmann::json> anime_get_raw(const std::string& name, short results = 1) noexcept {
-	std::string fname{ replace(name, ' ', "%20") };
-	std::unique_ptr<CURL, decltype(&curl_easy_cleanup)> curl(curl_easy_init(), curl_easy_cleanup);
-	std::string data{};
-	curl_easy_setopt(curl.get(), CURLOPT_URL, std::format("https://api.jikan.moe/{0}/anime?q=\"{1}\"&limit={2}", api_v, fname, results).c_str());
-	curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, write_data);
-	curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &data);
-	if (not std::ifstream{ ".\\cacert.pem" }) {
-		std::cout << "[MAL LOG] installing cacert.pem for accessing https://api.jikan.moe/" << std::endl;
-		install_cert();
-	}
-	curl_easy_setopt(curl.get(), CURLOPT_CAINFO, ".\\cacert.pem");
-	curl_easy_setopt(curl.get(), CURLOPT_CAPATH, ".\\cacert.pem");
-	if (curl_easy_perform(curl.get()) == CURLE_OK) {
-		nlohmann::json all_data = nlohmann::json::parse(std::move(data));
-		std::vector<nlohmann::json> j;
-		for (short i = 0; i < results and i < all_data["data"].size(); ++i)
-			j.push_back(all_data["data"][i]);
-		return j;
-	}
-	return {};
-}
-
 /* checks if the value is null. */
 template<typename T> T is_null(nlohmann::json& j) noexcept {
 	if (j.is_null()) return {};
 	return j.get<T>();
 }
 
-enum image_size { s_normal, s_small, s_large };
-enum type { t_tv, t_movie };
+enum image_size {
+	s_normal, /* normal size via MAL website */
+	s_small /* smaller than s_normal */,
+	s_large /* larger than s_normal */
+};
+
+enum type {
+	t_null, /* none mentioned */
+	t_tv, /* anime series */
+	t_special,
+	t_ona, /* Original Net Animation */
+	t_ova, /* Original Video Animation*/
+	t_movie /* anime movie*/
+};
 
 class anime {
 public:
+	nlohmann::json j{}; /* get specific objects from the API */
 	std::vector<std::string> image{}; /* the front image on MAL of that anime. enum: image_size */
 	std::string en_title{}; /* English title */
 	std::string jp_title{}; /* Japanese title */
@@ -88,7 +56,11 @@ public:
 				this->image.push_back(is_null<std::string>(this->j["images"]["jpg"][size]));
 			this->en_title = is_null<std::string>(this->j["title_english"]);
 			this->jp_title = is_null<std::string>(this->j["title_japanese"]);
-			this->type = (is_null<std::string>(this->j["type"]) == "Movie") ? type::t_movie : type::t_tv;
+			this->type = (is_null<std::string>(this->j["type"]) == "Movie") ? type::t_movie :
+				(is_null<std::string>(this->j["type"]) == "TV") ? type::t_tv :
+				(is_null<std::string>(this->j["type"]) == "Special") ? type::t_special :
+				(is_null<std::string>(this->j["type"]) == "ONA") ? type::t_ona :
+				(is_null<std::string>(this->j["type"]) == "OVA") ? type::t_ova : type::t_null;
 			this->episodes = is_null<short>(this->j["episodes"]);
 			this->airing = is_null<bool>(this->j["airing"]);
 			{
@@ -108,17 +80,58 @@ public:
 			this->background = is_null<std::string>(this->j["background"]);
 		}
 	}
-	/* return true if rate limited.
-	 * please take this to consideration as all anime information will be null if true.
+	/* please take this to consideration as all anime information will be null if true.
 	 * being aware of the rules is important! rate limit: 3req/sec
+	 * @return bool true if rate limited
 	*/
 	bool is_ratelimit() const noexcept {
 		return rate_limited;
 	}
 private:
-	nlohmann::json j{};
 	bool rate_limited{};
 };
+
+using anime_callback = std::function<void(const anime&)>;
+
+/* for CURLOPT_WRITEFUNCTION. this was made by libcurl not me. */
+size_t write_data(char* data, size_t size, size_t bytes, std::string* outcome) noexcept {
+	outcome->append(std::move(data), size * bytes);
+	return size * bytes;
+}
+
+/* replace string length data rather std::replace() replacing only char */
+std::string replace(const std::string& str, char replace, std::string with) noexcept {
+	std::string mstr{};
+	for (const char& c : str)
+		(c == replace) ? mstr += with : mstr += c;
+	return std::move(mstr);
+}
+
+/* GET a specific anime via name
+ * support spaces, mis-spelling. however this is not enhanced; wrong result is possible.
+ * @param results the number of results. this is useful for getting a whole anime series collection like one piece movies, fate series, ect
+ *   by default the value is 1 meaning it'll only GET the most relevant search based on the name.
+ * @param callback used to get all results once function is done
+*/
+void anime_get(const std::string& name, const int& results, anime_callback callback) {
+	std::unique_ptr<CURL, decltype(&curl_easy_cleanup)> curl(curl_easy_init(), curl_easy_cleanup);
+	std::string all_data{};
+	curl_easy_setopt(curl.get(), CURLOPT_URL, std::format("https://api.jikan.moe/{0}/anime?q=\"{1}\"&limit={2}",
+		api_v, replace(name, ' ', "%20"), results).c_str());
+	curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &all_data);
+	if (not std::ifstream{ ".\\cacert.pem" }) {
+		std::cout << "[MAL LOG] installing cacert.pem for accessing https://api.jikan.moe/" << std::endl;
+		install_cert();
+	}
+	curl_easy_setopt(curl.get(), CURLOPT_CAINFO, ".\\cacert.pem");
+	curl_easy_setopt(curl.get(), CURLOPT_CAPATH, ".\\cacert.pem");
+	if (curl_easy_perform(curl.get()) == CURLE_OK) {
+		nlohmann::json j = nlohmann::json::parse(std::move(all_data));
+		for (const auto& data : j["data"])
+			callback(std::move(anime(data)));
+	}
+}
 
 /* being worked on... */
 class manga {};
