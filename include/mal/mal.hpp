6da25a8494ae814dcad192dad4190e17784e3bab
LeeEndl <gavinolson71@gmail.com>
2024-01-14 20:49:13 -0600

    class migration & issue addressing
    this update wont effect performance however there are less lines and minor adjustments throughout the code including class simplicity and added comments!

--- include/mal/mal.hpp
+++ include/mal/mal.hpp
@@ -6,6 +6,8 @@
 #include <openssl/bio.h>
 #include <nlohmann/json.hpp>
 
+#include <iostream>
+
 using json = nlohmann::json;
 
 std::string replace(const std::string_view& str, char replace, const std::string& with) noexcept {
@@ -22,6 +24,7 @@ template<typename T> T is_null(const nlohmann::json& j) noexcept {
 
 using bio_callback = std::unique_ptr<BIO, decltype(&::BIO_free_all)>&;
 
+/* TODO: handle connection state REGARDING sudden unhandled crashes or stay alives. ~@LeeEndl */
 void request(std::function<void(bio_callback)> callback) {
 	std::unique_ptr<BIO, decltype(&::BIO_free_all)> bio{ BIO_new_ssl_connect(SSL_CTX_new(TLS_client_method())), ::BIO_free_all };
 	BIO_ctrl(bio.get(), BIO_C_SET_CONNECT, 0L, const_cast<char*>("api.jikan.moe:https"));
@@ -96,28 +99,16 @@ public:
 		this->synopsis = is_null<std::string>(j["synopsis"]);
 		this->background = is_null<std::string>(j["background"]);
 	}
-	constexpr ~anime() = default;
+	/* comments made by @LeeEndl. contact via github for any problems */
+	constexpr anime() = default; // initializes (0/null) variables disregarding JSON. used for lite/lightweight data parenting (used in manga)
+	constexpr ~anime() = default; // ^ same above but for deconstruct. nearly useless in this context but whatever
 private:
 };
 
-class manga {
+class manga : public anime {
 public:
-	int mal_id{};
-	std::vector<std::string> image{}; /* the front image on MAL of that manga. enum: image_size */
-	std::string title{}; /* recommended title. as some manga only have japanese title. */
-	std::string jp_title{}; /* Japanese title. */
 	int chapters{}; /* amount of chapters this manga contains. */
 	int volumes{}; /* amount of volumes this manga contains. */
-	bool publishing{}; /* if the manga is still being published. */
-	std::tm published{}; /* when the manga was published */
-	std::tm next_publish{}; /* when the manga's next published chapter */
-	double score{}; /* medium of overall score of this manga. e.g. 1.0 - 10.0 */
-	int scored_by{}; /* number of people who scored this manga. */
-	int rank{}; /* manga ranking. */
-	int popularity{}; /* the overall popularity of this manga. */
-	int favorites{}; /* total favorites. */
-	std::string synopsis{}; /* about the manga. */
-	std::string background{}; /* about the production. */
 	explicit manga(const json& j) noexcept {
 		this->mal_id = is_null<int>(j["mal_id"]);
 		for (const char* const& size : { "image_url", "small_image_url", "large_image_url" })
@@ -126,13 +117,13 @@ public:
 		this->jp_title = is_null<std::string>(j["title_japanese"]);
 		this->chapters = is_null<int>(j["chapters"]);
 		this->volumes = is_null<int>(j["volumes"]);
-		this->publishing = is_null<bool>(j["publishing"]);
-		this->published.tm_year = is_null<int>(j["published"]["prop"]["from"]["year"]);
-		this->published.tm_mon = is_null<int>(j["published"]["prop"]["from"]["month"]) + 1;
-		this->published.tm_mday = is_null<int>(j["published"]["prop"]["from"]["day"]);
-		this->next_publish.tm_year = is_null<int>(j["published"]["prop"]["to"]["year"]);
-		this->next_publish.tm_mon = is_null<int>(j["published"]["prop"]["to"]["month"]) + 1;
-		this->next_publish.tm_mday = is_null<int>(j["published"]["prop"]["to"]["day"]);
+		this->airing = is_null<bool>(j["publishing"]);
+		this->released.tm_year = is_null<int>(j["published"]["prop"]["from"]["year"]);
+		this->released.tm_mon = is_null<int>(j["published"]["prop"]["from"]["month"]) + 1;
+		this->released.tm_mday = is_null<int>(j["published"]["prop"]["from"]["day"]);
+		this->next_release.tm_year = is_null<int>(j["published"]["prop"]["to"]["year"]);
+		this->next_release.tm_mon = is_null<int>(j["published"]["prop"]["to"]["month"]) + 1;
+		this->next_release.tm_mday = is_null<int>(j["published"]["prop"]["to"]["day"]);
 		this->score = is_null<double>(j["score"]);
 		this->scored_by = is_null<int>(j["scored_by"]);
 		this->rank = is_null<int>(j["rank"]);
@@ -166,39 +157,41 @@ namespace mal {
 	void search(const std::string_view& name, const short& results, std::function<void(const T&)> callback) {
 		short i_results{};
 		for (short page = 1; page <= (results + 24) / 25; ++page) {
-			std::unique_ptr<json> j = std::make_unique<json>();
 			request([&](bio_callback bio) {
 				BIO_puts(bio.get(),
 				webpage(std::format(
 					"/v4/{0}?q=\"{1}\"&limit={2}&page={3}",
 					std::string(typeid(T).name()).substr(sizeof("class ") - 1, sizeof(typeid(T).name())), replace(name, ' ', "%20"), 25, page)).c_str()
 				);
+			std::unique_ptr<std::string> all_data = std::make_unique<std::string>();
 			{
-				std::unique_ptr<std::string> all_data = std::make_unique<std::string>();
-				{
-					std::unique_ptr<bool> once = std::make_unique<bool>(false);
-					std::unique_ptr<char[]> data = std::make_unique<char[]>(INT_MAX);
-					for (int r = BIO_read(bio.get(), data.get(), INT_MAX); r > 0; r = BIO_read(bio.get(), data.get(), INT_MAX)) {
-						data[r] = static_cast<char>(0);
-						all_data->append(data.get());
-						if (not *once and all_data->find("\r\n\r\n") not_eq -1) {
-							all_data = std::make_unique<std::string>(all_data->substr(all_data->find("\r\n\r\n") + 4));
-							once.reset(new bool(true));
-						}
+				std::unique_ptr<bool> once = std::make_unique<bool>(false);
+				std::unique_ptr<char[]> data = std::make_unique<char[]>(16384);
+				for (int r = BIO_read(bio.get(), data.get(), 16384); r > 0; r = BIO_read(bio.get(), data.get(), 16384)) {
+					data[r] = static_cast<char>(0);
+					all_data->append(data.get());
+					if (not *once and all_data->find("\r\n\r\n") not_eq -1) {
+						all_data = std::make_unique<std::string>(all_data->substr(all_data->find("\r\n\r\n") + 4));
+						once.reset(new bool(true));
 					}
-					std::erase_if(*all_data, [](const char& c) { return not std::isprint(c); });
 				}
-				all_data = std::make_unique<std::string>(all_data->substr(sizeof "\n\r\n", all_data->size() - sizeof "\n\r\n0"));
-				if (json::accept(*all_data))
-					*j = json(json::parse(*all_data));
+				std::erase_if(*all_data, [](const char& c) { return not std::isprint(static_cast<int>(c)); });
+			}
+			all_data = std::make_unique<std::string>(all_data->substr(sizeof "\n\r\n", all_data->size() - sizeof "\n\r\n0"));
+			try {
+				std::unique_ptr<json> j = std::make_unique<json>();
+				*j = json(json::parse(*all_data));
+				if ((*j)["data"].is_null()) return nullptr;
+				for (const json& data : (*j)["data"]) {
+					i_results++;
+					if (i_results > results) return nullptr;
+					callback(T(data));
+				}
+			}
+			catch (json::exception& e) {
+				if (e.id == 101) std::cout << "[MAL.cpp] ah oh! seems there is an error that is acknowledged by the developer... this issue will be treated soon as possible!" << std::endl;
 			}
 				});
-			if ((*j)["data"].is_null()) break;
-			for (const json& data : (*j)["data"]) {
-				i_results++;
-				if (i_results > results) break;
-				callback(T(data));
-			}
 		}
 	}
 }
